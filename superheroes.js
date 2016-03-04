d7068dcdb42e43d7bca4b99dfac72ac26b1c4d4d
DanLaskarzewski <dlaskarzewski@ccsuvt.org>
2016-03-04 09:44:44 -0500

    names stuff

--- superheroes.js
+++ superheroes.js
@@ -15,3 +15,36 @@ var superheroes = [{
   Color: "red",
   Superpower: "SHAZAM!"
 }];
+
+
+
+/**********
+NAMES
+**********/
+
+/* VARS AND FINDS NAMES*/
+var message = '';
+var showOrHide = 0;
+
+/*message += "<h2>Names: </h2>"*/
+  
+for (var name in superheroes){
+  message += '<p>' + superheroes[name].Name + '</p>';
+}
+
+/*PRINTS THE LIST*/
+var nameDisplay = document.getElementById('buttonNames');
+
+nameDisplay.onclick = function () {
+  showOrHide++;
+  var outputDiv = document.getElementById('outputNames');
+  outputDiv.innerHTML = message;
+  
+  /*EVERY OTHER CLICK DISPLAYS THE LIST*/
+  if (showOrHide % 2 != 0){  
+    outputDiv.style.display = "block";
+  }
+  else {
+    outputDiv.style.display = "none";
+  }
+}
