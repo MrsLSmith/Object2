73cc6c7319acbc44c4b3a93a2550105a31581f43
CollynHawes <chawes@ccsuvt.org>
2016-03-08 09:37:41 -0500

    Collyn

--- superheroes.js
+++ superheroes.js
@@ -1,3 +1,9 @@
+var countTable = 0;
+var HeroesTable1 = "";
+var HeroesTable2 = "";
+var HeroesTable3 = "";
+var Heroes = "";
+var HeroStuff = "";
 var superheroes = [{
   Name: "Flash",
   Color: "red",
@@ -15,3 +21,24 @@ var superheroes = [{
   Color: "red",
   Superpower: "SHAZAM!"
 }];
+for (var i = 0; i < superheroes.length; i += 1) {
+  Heroes = superheroes[i];
+  HeroesTable1 += "<p class='rows'>" + Heroes.Name + "</p>";
+  HeroesTable2 += "<p class='rows'>" + Heroes.Color + "</p>";
+  HeroesTable3 += "<p class='rows'>" + Heroes.Superpower + "</p>";
+  document.getElementById("TableCollumOne").innerHTML = HeroesTable1;
+  document.getElementById("TableCollumTwo").innerHTML = HeroesTable2;
+  document.getElementById("TableCollumThree").innerHTML = HeroesTable3;
+}
+
+var clickThing =
+  document.getElementById('HeroTable');
+clickThing.style.cursor = 'pointer';
+clickThing.onclick = function() {
+  countTable += 1;
+  if (countTable % 2 === 0) {
+    document.getElementById("Table").style.display = 'none';
+  } else {
+    document.getElementById("Table").style.display = 'block';
+  }
+}
